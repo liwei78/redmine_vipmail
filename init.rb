23fa550538c2e17da2359d31207bbd8b0144197a
liwei <hi@liwei.me>
2012-07-12 22:31:25 +0800

    first commit

---
+++ init.rb
@@ -0,0 +1,14 @@
+# liwei created at 2012-7-12
+# 0.0.1 basic
+require 'redmine'
+
+Redmine::Plugin.register :redmine_vipmail do
+  name 'Redmine VIP Mail'
+  author 'Riquel Li'
+  description 'Fix something'
+  version '0.0.1'
+  url 'http://railser.cn'
+  author_url 'http://railser.cn/me'
+
+end
+
